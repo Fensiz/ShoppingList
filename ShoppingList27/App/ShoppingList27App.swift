2b19778fe0999272af56a1ffad7366f4ae398ca2
Nikita Tsomuk <tsomuk@me.com>
2025-09-01 14:31:52 +0300

    [Refactor] Initial Commit

---
+++ ShoppingList27/App/ShoppingList27App.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingList27App.swift
+//  ShoppingList27
+//
+//  Created by Nikita Tsomuk on 01.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct ShoppingList27App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
