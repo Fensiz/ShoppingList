37b67662a8fe46f404f69acd98f959b00da6d626
Симонов Иван дмитриевич <fensiz@me.com>
2025-09-08 21:19:21 +0300

    [feature] added raw version of ShoppingListView

--- ShoppingList27/Models/ListItem.swift
+++ ShoppingList27/Models/ListItem.swift
@@ -6,6 +6,19 @@
 //
 
 import Foundation
+import SwiftData
+
+struct Product: Identifiable, Hashable {
+	let name: String
+	var count: Int
+	var unit: Unit
+	var isBought: Bool
+	var id: String { name }
+
+	static func == (lhs: Product, rhs: Product) -> Bool {
+		lhs.name == rhs.name
+	}
+}
 
 struct ListItem: Identifiable, Hashable {
 	let logo: ItemLogo
@@ -37,3 +50,36 @@ struct ListItem: Identifiable, Hashable {
 		total: 20
 	)
 }
+
+//@Model
+//final class ListItem2: Identifiable, Hashable {
+//	var logo: ItemLogo
+//	@Attribute(.unique)
+//	var name: String
+////	@Relationship(delete: .cascade, update: .cascade)
+//	var products: [Product]
+//	var id: String { name }
+//
+//	init(logo: ItemLogo, name: String, products: [Product]) {
+//		self.logo = logo
+//		self.name = name
+//		self.products = products
+//	}
+//}
+//
+//
+//@Model
+//final class Product: Hashable {
+//	@Attribute(.unique) var name: String
+//	var count: Int
+//	var unit: String
+//	var isBought: Bool
+//	var id: String { name }
+//
+//	init(name: String, count: Int, unit: String, isBought: Bool) {
+//		self.name = name
+//		self.count = count
+//		self.unit = unit
+//		self.isBought = isBought
+//	}
+//}
